e8ec3a9367f8c2e7dda6fb502ed5999f02038736
Jukoleda <30731754+Jukoleda@users.noreply.github.com>
2019-06-24 15:55:11 -0300

    Updates.

--- probot.ts
+++ probot.ts
@@ -43,7 +43,7 @@ enum Colores {
 }
 
 //% weight=5 color=#ff8000 icon="\uf2db"
-//% groups="['Leds', 'Motores','Buzzer']"
+//% groups="['Leds', 'Motores','Buzzer','Ultrasonido']"
 namespace probot {
 
     export class TiraDeLeds {
@@ -457,5 +457,46 @@ namespace probot {
         let duration = duracion < 0 ? 0 : duracion;
         pins.analogPitch(frequency, duration)
     }
+    enum PingUnit {
+        //% block="μs"
+        MicroSeconds,
+        //% block="cm"
+        Centimeters,
+        //% block="inches"
+        Inches
+    }
+
+    /**
+     * Sonar and ping utilities
+     */
+    //% color="#2c3e50" weight=10
+    /**
+     * Send a ping and get the echo time (in microseconds) as a result
+     * @param trig tigger pin
+     * @param echo echo pin
+     * @param unit desired conversion unit
+     * @param maxCmDistance maximum distance in centimeters (default is 500)
+     */
+    //% blockId=sonar_ping block="ping trig %trig|echo %echo|unit %unit"
+    //% group="Ultrasonido"
+    export function ping(trig: DigitalPin, echo: DigitalPin, unit: PingUnit, maxCmDistance = 500): number {
+        // send pulse
+        pins.setPull(trig, PinPullMode.PullNone);
+        pins.digitalWritePin(trig, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(trig, 1);
+        control.waitMicros(10);
+        pins.digitalWritePin(trig, 0);
+
+        // read pulse
+        const d = pins.pulseIn(echo, PulseValue.High, maxCmDistance * 58);
+
+        switch (unit) {
+            case PingUnit.Centimeters: return Math.idiv(d, 58);
+            case PingUnit.Inches: return Math.idiv(d, 148);
+            default: return d;
+        }
+    }
+    
 
 }
