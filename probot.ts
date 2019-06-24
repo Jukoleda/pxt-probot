b79f2f473d79db37f1f4c4450882e2c26320ff91
Jukoleda <30731754+Jukoleda@users.noreply.github.com>
2019-06-24 16:07:30 -0300

    26062019v2

--- probot.ts
+++ probot.ts
@@ -477,6 +477,7 @@ namespace probot {
     //% blockId=sonar_ping block="ping trig %trig|echo %echo|unit %unit"
     //% group="Ultrasonido"
     //% inlineInputMode=inline
+    //%blockSetVariable=distancia
     export function ping(trig: DigitalPin, echo: DigitalPin, unit: PingUnit, maxCmDistance = 500): number {
         // send pulse
         pins.setPull(trig, PinPullMode.PullNone);
