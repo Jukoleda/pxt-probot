0f9a51e0071c7fbaac14a4dd703030adc8868244
Jukoleda <30731754+Jukoleda@users.noreply.github.com>
2021-06-10 17:35:07 -0300

    How to test color sensor

--- probotest.ts
+++ probotest.ts
@@ -1,1 +1,7 @@
 basic.showNumber(probots.ping(probots.conexiones_ret(conn.CON1)))
+
+
+probots.initColorSensor()
+if (probots.getSensedColorValue() == probots.colors_ret(Names_colors.White)) {
+    basic.showString("WHITE")
+}
